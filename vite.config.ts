28647c3eb2c5ac5f65fbc26694c0ee9d0452644f
dailymodua@gmail.com <dailymodua@gmail.com>
2026-04-07 13:33:00 +0200

    Enable GitHub Pages preview

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/ecofactorautop/",
+  base: mode === "development" ? "/" : "/ECOFACTOR/",
   server: {
     host: "::",
     port: 8080,
